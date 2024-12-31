4e6ee33c56ebc7e2f06ecbbf717257fac07d6931
amybui0910 <amybui0910@gmail.com>
2024-12-31 16:50:31 -0500

    updated handtracker.py

--- handtracker.py
+++ handtracker.py
@@ -27,17 +27,20 @@ while True:
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                print(id, cx, cy)
-                
-                if id == 4:
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                print(id, cx, cy)  
                 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
     cTime = time.time()
-    fps = 1/(cTime - ptime)
+    fps = 1 / (cTime - ptime)
     ptime = cTime
     
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
     
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    
+    # Quit loop when the 'esc' key is pressed
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
